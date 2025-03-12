b6ca178c8aebb2d8d1f8a63ac9126459745b1a7a
denis-mwanzia <denismwanzia56@gmail.com>
2025-03-12 19:28:05 +0300

    assignment 1 DONE

---
+++ WEEK1/assignment1.py
@@ -0,0 +1,29 @@
+def calculator():
+    #Get user input for two numbers
+    firstNumber = int(input("Enter First Number:"))
+    secondNumber = int(input("Enter Second Number:"))
+    
+    #Get user input for operator
+    operator = input("Enter Operator (+, -, *, /):")
+    
+    #Perform calculations based on the operator
+    if operator == '+':
+        result = firstNumber + secondNumber
+    elif operator == '-':
+        result = firstNumber - secondNumber
+    elif operator == '*':
+        result = firstNumber * secondNumber
+    elif operator == '/':
+        # Check for division by zero
+        if secondNumber == 0:
+            print("Error: Division by zero is not allowed.")
+            return
+        result = firstNumber / secondNumber
+    else:
+        print("Error: Invalid operator. Please enter a valid operator (+, -, *, /).")
+        return
+    # Display the result
+    print(f"firstNumber {operator} secondNumber = {result}")
+    
+#Calling the function
+calculator()    
